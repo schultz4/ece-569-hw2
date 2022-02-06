6ec00fe42dd95c08f4ba8d20d5600ac29c1ff14f
Christopher Schultze <schultz4@i16n0.ocelote.hpc.arizona.edu>
2022-02-06 14:11:25 -0700

    First pass hw2 task1. Untested

--- hw2/VectorAdd/solution.cu
+++ hw2/VectorAdd/solution.cu
@@ -3,6 +3,14 @@
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   //@@ Insert code to implement vector addition here
   //   and launch your kernel from the main function
+    int tid = blockDim.x*blockIdx.x + threadIdx.x;
+
+    if (tid >= len) 
+    {
+        return;
+    }
+    printf("my thread id is %d and block id is %d\n",threadIdx.x, blockIdx.x);
+    out[tid] = in1[tid] + in2[tid];
 }
 
 int main(int argc, char **argv) {
@@ -29,29 +37,70 @@ int main(int argc, char **argv) {
 
   wbTime_start(GPU, "Allocating GPU memory.");
   //@@ Allocate GPU memory here
+  if (cudaMalloc((void **) &deviceInput1, inputLength*sizeof(float)) != cudaSuccess) {
+      printf("malloc error for deviceInput1\n");
+      return 0;
+  }
+  if (cudaMalloc((void **) &deviceInput2, inputLength*sizeof(float)) != cudaSuccess) {
+      printf("malloc error for deviceInput2\n");
+      return 0;
+  }
+  if (cudaMalloc((void **) &deviceOutput, inputLength*sizeof(float)) != cudaSuccess) {
+      printf("malloc error for deviceOutput\n");
+      return 0;
+  }
 
   wbTime_stop(GPU, "Allocating GPU memory.");
 
   wbTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
+  if (cudaMemcpy(deviceInput1,hostInput1,inputLength*sizeof(float),cudaMemcpyHostToDevice) != cudaSuccess){
+      cudaFree(deviceInput1);
+      cudaFree(deviceInput2);
+      cudaFree(deviceOutput);
+      free(hostInput1);
+      free(hostInput2);
+      free(hostOutput);
+      printf("data transfer error from host to device on deviceInput1\n");
+      return 0;
+  }
+  if (cudaMemcpy(deviceInput2,hostInput2,inputLength*sizeof(float),cudaMemcpyHostToDevice) != cudaSuccess){
+      cudaFree(deviceInput1);
+      cudaFree(deviceInput2);
+      cudaFree(deviceOutput);
+      free(hostInput1);
+      free(hostInput2);
+      free(hostOutput);
+      printf("data transfer error from host to device on deviceInput2\n");
+      return 0;
+  }
 
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
+  dim3 mygrid(ceil(inputLength/256.0));
+  dim3 myblock(256);
 
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
+  vecAdd<<<mygrid,myblock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
 
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Performing CUDA computation");
 
   wbTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
+  if (cudaMemcpy(hostOutput,deviceOutput,inputLength*sizeof(float),cudaMemcpyDeviceToHost) != cudaSuccess){
+      printf("data transfer error from device to host on deviceOutput\n");
+  }
 
   wbTime_stop(Copy, "Copying output memory to the CPU");
 
   wbTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
 
   wbTime_stop(GPU, "Freeing GPU Memory");
 
